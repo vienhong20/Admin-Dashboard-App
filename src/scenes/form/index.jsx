cf7f2aee6c44cc3935a3d4ea388e07171e5b976f
VIEN HONG <vienhong88@gmail.com>
2023-02-16 09:47:55 -0500

    Update Calendar
    Work in progress

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -130,6 +130,11 @@ const Form = () => {
                         sx={{ gridColumn: "span 4" }}
                         />
                     </Box>
+                    <Box display="flex" justifyContent="end" mt="20px">
+                        <Button type="submit" color="secondary" variant="contained">
+                            Create New User
+                        </Button>
+                    </Box>
                 </form>
             )};
         </Forkmik>
